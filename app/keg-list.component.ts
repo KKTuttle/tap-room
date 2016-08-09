2fde521181969edb179b294017cfce85da26332f
Caleb Stokka and Leah Nelson <caleb+leah@>
2016-08-09 10:10:57 -0700

    new keg form

--- app/keg-list.component.ts
+++ app/keg-list.component.ts
@@ -2,13 +2,13 @@ import { Component, EventEmitter } from 'angular2/core';
 import { KegComponent } from './keg.component';
 import { Keg } from './keg.model';
 import { EditKegDetailsComponent} from './edit-keg-details.component';
-// import { NewKegComponent } from './new-keg.component';
+import { NewKegComponent } from './new-keg.component';
 //
 @Component({
   selector: 'keg-list',
   inputs: ['kegList'],
   outputs: ['onKegSelect'],
-  directives: [KegComponent, EditKegDetailsComponent],
+  directives: [KegComponent, EditKegDetailsComponent, NewKegComponent],
   template: `
   <keg-display *ngFor="#currentKeg of kegList"
     (click)="kegClicked(currentKeg)"
@@ -17,6 +17,7 @@ import { EditKegDetailsComponent} from './edit-keg-details.component';
   </keg-display>
   <edit-keg-details *ngIf="selectedKeg"[keg]="selectedKeg">
   </edit-keg-details>
+  <new-keg (onSubmitNewKeg)="createKeg($event.name, $event.brand, $event.price, $event.alcoholContent)"></new-keg>
   `
 })
 export class KegListComponent {
@@ -30,5 +31,10 @@ export class KegListComponent {
     console.log('child', clickedKeg);
     this.selectedKeg = clickedKeg;
     this.onKegSelect.emit(clickedKeg);
+  }
+  createKeg(name: string, brand: string, price: number, alcoholContent: number): void {
+    this.kegList.push(
+      new Keg(name, brand, price, alcoholContent, this.kegList.length)
+    );
   }
 }
--- app/new-keg.component.ts
+++ app/new-keg.component.ts
@@ -1,1 +1,35 @@
-// import { Keg } from './keg.model';
+import { Component, EventEmitter } from 'angular2/core';
+import { Keg } from './keg.model';
+
+@Component({
+  selector: 'new-keg',
+  outputs: ['onSubmitNewKeg'],
+  template:`
+    <div class='keg-form'>
+      <h3>Add a new keg:</h3>
+        <input placeholder="Name" class="col-sm-8 input-lg" #newName>
+        <input placeholder="Brand" class="col-sm-8 input-lg" #newBrand>
+        <input placeholder="Price" class="col-sm-8 input-lg" #newPrice>
+        <input placeholder="Alcohol Content" class="col-sm-8 input-lg" #newAlcoholContent>
+        <button (click)="addKeg(newName, newBrand, newPrice, newAlcoholContent)" class="btn-warning btn-lg add-button">Add</button>
+    </div>
+  `
+})
+export class NewKegComponent {
+  public onSubmitNewKeg: EventEmitter<Object>;
+  constructor(){
+    this.onSubmitNewKeg = new EventEmitter();
+  }
+  addKeg(userName: HTMLInputElement, userBrand: HTMLInputElement, userPrice: HTMLInputElement, userAlcoholContent: HTMLInputElement) {
+    this.onSubmitNewKeg.emit({
+      name: userName.value,
+      brand: userBrand.value,
+      price: userPrice.value,
+      alcoholContent: userAlcoholContent.value
+    });
+    userName.value = "";
+    userBrand.value = "";
+    userPrice.value = "";
+    userAlcoholContent.value = "";
+  }
+}
